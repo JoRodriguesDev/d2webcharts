cf2414725715a67e41e0501016d554f8d9528a5e
Joseildo Rodrigues <142036947+JoRodriguesDev@users.noreply.github.com>
2025-03-18 16:37:59 -0300

    Update Labels
    Update Labels in function Update: string;

--- uModel.Charts.Radar.pas
+++ uModel.Charts.Radar.pas
@@ -112,16 +112,23 @@ end;
 function TModelChartRadar.Update: string;
 begin
   var LDataSetUpdateStr := '';
+  var LLabelsUpdateStr  := '';
+
   for var i := 0 to FChartDataSets.Count - 1 do
   begin
-    var LDatasetsStr := (FChartDataSets[i] as iModelChartDataSet).ArrayValues;
+    var LChartDataSet   := (FChartDataSets[i] as iModelChartDataSet);
+    var LDatasetsStr    := LChartDataSet.ArrayValues;
+    var LDatasetLabels  := LChartDataSet.GenerateLabels;
+
     LDataSetUpdateStr := LDataSetUpdateStr + Format('chart.data.datasets[%d].data = %s;', [i, LDatasetsStr]);
+    LLabelsUpdateStr  := LLabelsUpdateStr + Format('chart.data.datasets[%d].labels = [%s];', [i, LDatasetLabels]);
   end;
 
   Result :=
     'var chart = Chart.getChart("'+ FChartID +'");' +
     'if (chart) {' +
-    ' ' + LDataSetUpdateStr + ' ' +
+    '  ' + LLabelsUpdateStr +
+    '  ' + LDataSetUpdateStr +
     '  chart.update();' +
     '}';
 end;
