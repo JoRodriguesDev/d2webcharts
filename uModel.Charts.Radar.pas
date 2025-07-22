9887dddea8168d755c0dbdecbdb98bf0e155d284
Joseildo Rodrigues <142036947+JoRodriguesDev@users.noreply.github.com>
2025-07-22 12:52:12 -0300

    Update uModel.Charts.Radar.pas

--- uModel.Charts.Radar.pas
+++ uModel.Charts.Radar.pas
@@ -151,10 +151,10 @@ begin
   end;
 
   Result := Format(
-    '<canvas id="chartjs-radar'+ FChartID +'" width="%s" height="%s"></canvas>' +
+    '<canvas id="'+ FChartID +'" width="%s" height="%s"></canvas>' +
     '<script>' +
     'document.addEventListener("DOMContentLoaded", () => {' +
-    '  new Chart(document.getElementById("chartjs-radar' + FChartID + '"), {' +
+    '  new Chart(document.getElementById("' + FChartID + '"), {' +
     '    type: "radar",' +
     '    data: {' +
     '      labels: [%s],' +
