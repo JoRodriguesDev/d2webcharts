eeabbe01c0a63e9cdef9fc8b5111ffcd45f308fb
Joseildo Rodrigues <142036947+JoRodriguesDev@users.noreply.github.com>
2025-07-22 12:49:45 -0300

    Update uModel.Charts.Doughnut.pas

--- uModel.Charts.Doughnut.pas
+++ uModel.Charts.Doughnut.pas
@@ -151,10 +151,10 @@ begin
   end;
 
   Result := Format(
-    '<canvas id="chartjs-doughnut'+ FChartID +'" width="%s" height="%s"></canvas>' +
+    '<canvas id="'+ FChartID +'" width="%s" height="%s"></canvas>' +
     '<script>' +
     'document.addEventListener("DOMContentLoaded", () => {' +
-    '  new Chart(document.getElementById("chartjs-doughnut' + FChartID + '"), {' +
+    '  new Chart(document.getElementById("' + FChartID + '"), {' +
     '    type: "doughnut",' +
     '    data: {' +
     '      labels: [%s],' +
